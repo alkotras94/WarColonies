9d766adea814b7ea1fe761ee973ff0cb3e6c01e0
alkotras94 <104847958+alkotras94@users.noreply.github.com>
2025-04-23 10:53:20 +0500

    Create Unit Save Mac

--- Assets/Scripts/View/ReceiverView.cs
+++ Assets/Scripts/View/ReceiverView.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReceiverView : MonoBehaviour, IHitble
 {
+    [SerializeField] private UnitFactory _unitfactory;
     //type to locate the reception area for new units
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PartFood food))
+        {
+            _unitfactory.CanSpawn();
+        }
+    }
 }
